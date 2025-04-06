9a30ca1c18e7061264b77a61c826958a13b4a0ee
lLana <pp71802428@gmail.com>
2025-04-06 20:35:06 +0700

    update

--- config/banner.js
+++ config/banner.js
@@ -11,7 +11,7 @@ function displayBanner() {
     })
     console.log(chalk.green(banner));
     console.log(chalk.yellow('========================================='));
-    console.log(chalk.magenta('Github   : https://github.com/CryptoKidzz'));
+    console.log(chalk.magenta('Github   : https://github.com/0x-Disciple'));
     console.log(chalk.magenta('Telegram : https://t.me/CryptoKidzz'));
     console.log(chalk.yellow('========================================='));
 }
